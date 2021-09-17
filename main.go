88b98c764427a16c795c479675e6ed7c0179cfa7
JeanLeRoux <jeanlr14@gmail.com>
2021-09-17 20:38:15 +0200

    add api call for crypto details

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
@@ -17,52 +19,113 @@ type article struct {
 	Body       string
 }
 
-type crypto struct {
-	Name              string
-	Symbol            string
-	PriceUSD          string
-	VolumeUSD         string
-	MarketCapacityUSD string
-	Change1h          string
-	Change24h         string
-	Change7d          string
+type cryptoListResponse struct {
+	Data cryptoList `json:"data"`
+}
+
+type cryptoList struct {
+	CryptoCurrencyList []cryptoDetails `json:"cryptoCurrencyList"`
+	TotalCount         string          `json:"totalCount"`
+}
+
+type cryptoDetails struct {
+	Id                int            `json:"id"`
+	Name              string         `json:"name"`
+	Symbol            string         `json:"symbol"`
+	Slug              string         `json:"slug"`
+	CmcRank           int            `json:"cmcRank"`
+	MarketPairCount   float64        `json:"marketPairCount"`
+	CirculatingSupply float64        `json:"circulatingSupply"`
+	TotalSupply       float64        `json:"totalSupply"`
+	MaxSupply         float64        `json:"maxSupply"`
+	Ath               float64        `json:"ath"`
+	Atl               float64        `json:"atl"`
+	High24h           float64        `json:"high24h"`
+	Low24h            float64        `json:"low24h"`
+	IsActive          int            `json:"isActive"`
+	LastUpdated       string         `json:"lastUpdated"`
+	DateAdded         string         `json:"dateAdded"`
+	Quotes            []cryptoQuotes `json:"quotes"`
+}
+
+type cryptoQuotes struct {
+	Name                     string  `json:"name"`
+	Price                    float64 `json:"price"`
+	Volume24h                float64 `json:"volume24h"`
+	Volume7d                 float64 `json:"volume7d"`
+	Volume30d                float64 `json:"volume30d"`
+	MarketCap                float64 `json:"marketCap"`
+	PercentChange1h          float64 `json:"percentChange1h"`
+	PercentChange24h         float64 `json:"percentChange24h"`
+	PercentChange7d          float64 `json:"percentChange7d"`
+	LastUpdated              float64 `json:"lastUpdated"`
+	PercentChange30d         float64 `json:"percentChange30d"`
+	PercentChange60d         float64 `json:"percentChange60d"`
+	PercentChange90d         float64 `json:"percentChange90d"`
+	FullyDilluttedMarketCap  float64 `json:"fullyDilluttedMarketCap"`
+	MarketCapByTotalSupply   float64 `json:"marketCapByTotalSupply"`
+	Dominance                float64 `json:"dominance"`
+	Turnover                 float64 `json:"turnover"`
+	YtdPriceChangePercentage float64 `json:"ytdPriceChangePercentage"`
+}
+
+type cryptoMetadata struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Symbol  string `json:"symbol"`
+	CmcRank int    `json:"cmcRank"`
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/Tech", getLatestTech)
-	router.GET("/Crypto", getLatestCrypto)
+	router.GET("/CryptoMeta", getCryptoMetadata)
 	router.Run("localhost:8000")
 }
 
-func getLatestCrypto(ginReturn *gin.Context) {
-	cryptos := []crypto{}
-	c := colly.NewCollector()
-	c.OnError(func(_ *colly.Response, err error) {
+func getCryptoMetadata(ginReturn *gin.Context) {
+	resp, err := http.Get("https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start=1&limit=100&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false&aux=ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,max_supply,circulating_supply,total_supply,volume_7d,volume_30d")
+	if err != nil {
 		fmt.Println(err)
-	})
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("visiting", r.URL.String())
-	})
-	c.OnHTML("tbody>tr", func(e *colly.HTMLElement) {
-		if len(e.ChildText(".cmc-table__column-name--name")) != 0 {
-			temp := crypto{
-				Name:              e.ChildText(".cmc-table__column-name--name"),
-				Symbol:            e.ChildText(".cmc-table__cell--sort-by__symbol"),
-				PriceUSD:          e.ChildText(".cmc-table__cell--sort-by__price"),
-				VolumeUSD:         e.ChildText(".cmc-table__cell--sort-by__volume-24-h"),
-				MarketCapacityUSD: e.ChildText(".cmc-table__cell--sort-by__market-cap>p>.sc-1ow4cwt-1"),
-				Change1h:          e.ChildText(".cmc-table__cell--sort-by__percent-change-1-h"),
-				Change24h:         e.ChildText(".cmc-table__cell--sort-by__percent-change-24-h"),
-				Change7d:          e.ChildText(".cmc-table__cell--sort-by__percent-change-7-d"),
-			}
-			cryptos = append(cryptos, temp)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var cryptoListResp cryptoListResponse
+	json.Unmarshal(body, &cryptoListResp)
+	var cryptoMeta []cryptoMetadata
+	for _, value := range cryptoListResp.Data.CryptoCurrencyList {
+		temp := cryptoMetadata{
+			Id:      value.Id,
+			Name:    value.Name,
+			Symbol:  value.Symbol,
+			CmcRank: value.CmcRank,
 		}
-	})
+		cryptoMeta = append(cryptoMeta, temp)
+	}
+	ginReturn.IndentedJSON(http.StatusOK, cryptoMeta)
 
-	c.Visit("https://coinmarketcap.com/all/views/all/")
-	ginReturn.IndentedJSON(http.StatusOK, cryptos)
 }
+
+// func getCryptoHistoricData(ginReturn *gin.Context) {
+// 	news := []article{}
+// 	c := colly.NewCollector()
+// 	detailCollector := c.Clone()
+// 	c.OnError(func(_ *colly.Response, err error) {
+// 		fmt.Println(err)
+// 	})
+// 	c.OnRequest(func(r *colly.Request) {
+// 		fmt.Println("visiting", r.URL.String())
+// 	})
+// 	c.OnHTML(".article-listing", func(e *colly.HTMLElement) {
+// 		e.ForEach("article", func(i int, h *colly.HTMLElement) {
+// 			url := h.ChildAttr(".tf-article-url", "href")
+// 			URL := h.Request.AbsoluteURL(url)
+// 			detailCollector.Visit(URL)
+// 		})
+// 	})
+// 	c.Visit("https://www.businessinsider.co.za/tech")
+// 	ginReturn.IndentedJSON(http.StatusOK, news)
+// }
 
 func getLatestTech(ginReturn *gin.Context) {
 	news := []article{}
