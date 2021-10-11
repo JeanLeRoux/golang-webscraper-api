dc008c5979271380ed0646af91143ee7d1535f6b
JeanLeRoux <54205091+JeanLeRoux@users.noreply.github.com>
2021-10-11 16:17:27 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,5 +10,5 @@ func main() {
 	router.GET("/CryptoMeta", getCryptoMetadata)
 	router.GET("/CryptoNews", getCryptoNews)
 	router.GET("/CryptoChart", getCryptoChartData)
-	router.Run("https://go-crypto-backend.herokuapp.com:8000")
+	router.Run(":" + port)
 }
